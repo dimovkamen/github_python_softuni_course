52ab1b750405e6ba6a53a4c2420418ec03b8df18
Kamen Dimov <Kamen.Dimov@playtech.com>
2024-01-20 15:25:48 +0200

    Add course files

---
+++ fundamentals/02_data_types_and_variables/00_test.py
@@ -0,0 +1,17 @@
+# list1 = [123, 0.5, 'bus', "A"]
+# print(list1[0])
+# print(list1[1])
+# print(list1[2])
+# print(list1[3])
+# print()
+# list1[2] = "Car"
+# print(list1[0])
+# print(list1[1])
+# print(list1[2])
+# print(list1[3])
+
+dictionary1 = {'name': 'Kamen', 'age': 23, 'height': 180.3}
+
+print(dictionary1['name'])
+print(dictionary1['age'])
+print(dictionary1['height'])
